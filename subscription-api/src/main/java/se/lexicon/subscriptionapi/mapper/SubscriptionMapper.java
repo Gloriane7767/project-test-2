9b226cfd7cd049a7fe324db86d6cd086adabb1a7
GLORY NGUM FONCHA <gloriane7767@gmail.com>
2026-03-17 14:28:01 +0100

    add Plan and Subscription Mapper interfaces

---
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/mapper/SubscriptionMapper.java
@@ -0,0 +1,15 @@
+package se.lexicon.subscriptionapi.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import se.lexicon.subscriptionapi.domain.entity.Subscription;
+import se.lexicon.subscriptionapi.dto.response.SubscriptionResponse;
+
+@Mapper(componentModel = "spring")
+public interface SubscriptionMapper {
+    @Mapping(source = "plan.id", target = "planId")
+    @Mapping(source = "plan.name", target = "planName")
+    @Mapping(source = "plan.serviceType", target = "serviceType")
+    @Mapping(source = "plan.operator.name", target = "operatorName")
+    SubscriptionResponse toResponse(Subscription subscription);
+}
