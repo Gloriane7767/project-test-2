8b3841994a8eb8babc2767b80f77423ba093fe98
GLORY NGUM FONCHA <gloriane7767@gmail.com>
2026-03-17 14:09:06 +0100

    add Plan and Subscription Repositories

---
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/repository/PlanRepository.java
@@ -0,0 +1,15 @@
+package se.lexicon.subscriptionapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import se.lexicon.subscriptionapi.domain.constant.ServiceType;
+import se.lexicon.subscriptionapi.domain.entity.Plan;
+
+import java.util.List;
+
+@Repository
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+    List<Plan> findByActiveTrue();
+    List<Plan> findByActiveTrueAndServiceType(ServiceType serviceType);
+    List<Plan> findByOperatorId(Long operatorId);
+}
